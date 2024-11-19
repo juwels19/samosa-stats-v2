c0d5d2b5e3f011d7c37265579b457fa846a347f8
Julian Curalli <julian.curalli27@gmail.com>
2024-11-18 21:43:01 -0500

    fixed loading states

--- src/app/settings/_components/categories/category-management-loading.tsx
+++ src/app/settings/_components/categories/category-management-loading.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Skeleton } from "~/components/ui/skeleton";
 
 const CategoryManagementLoading = () => {
-  return <div>CategoryManagementLoading</div>;
+  return (
+    <Card className="h-full">
+      <CardHeader>
+        <CardTitle className="flex justify-between gap-4">Season</CardTitle>
+        <CardDescription>
+          <Skeleton className="h-4 w-1/2" />
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <Skeleton className="h-56 w-full mt-2" />
+        <Skeleton className="h-9 w-1/4" />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default CategoryManagementLoading;
--- src/app/settings/_components/events/event-management-loading.tsx
+++ src/app/settings/_components/events/event-management-loading.tsx
@@ -1,7 +1,61 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Skeleton } from "~/components/ui/skeleton";
+
+const EventCardLoading = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <Skeleton className="h-6 w-full" />
+        </CardTitle>
+        <CardDescription>
+          <Skeleton className="h-3 w-20" />
+          <Skeleton className="mt-2 h-3 w-40" />
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-cols-1 gap-2">
+          <Skeleton className="h-4 w-40" />
+          <Skeleton className="h-4 w-48" />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <div className="w-full flex flex-row justify-between items-center">
+          <Skeleton className="h-4 w-20" />
+          <Skeleton className="h-8 w-20" />
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
 
 const EventManagementLoading = () => {
-  return <div>EventManagementLoading</div>;
+  return (
+    <Card className="h-full">
+      <CardHeader>
+        <CardTitle className="flex justify-between items-center gap-4">
+          Events
+          <Skeleton className="h-9 w-28" />
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <EventCardLoading />
+          <EventCardLoading />
+          <EventCardLoading />
+          <EventCardLoading />
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default EventManagementLoading;
